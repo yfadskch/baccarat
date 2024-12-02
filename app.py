8a9e5092838b3e73371fe69322f0ee705a2fa4c3
yfadskch <yfadskch@gmail.com>
2024-12-03 01:45:38 +0800

    1

--- app.py
+++ app.py
@@ -1,55 +1,59 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import random
 
 app = Flask(__name__)
 
-# 初始余额
-player_balance = 1000
-
-# 游戏历史记录
+player_balance = 2000
 game_history = []
 
-@app.route('/', methods=['GET', 'POST'])
-def baccarat_game():
-    global player_balance
-    global game_history
+def draw_card():
+    """抽一张卡，返回卡牌和点数"""
+    card = random.randint(1, 9)  # 卡牌是 1 到 9
+    return str(card), card  # 返回卡牌和点数
 
-    if request.method == 'POST':
-        # 获取用户下注金额
-        try:
-            bet = int(request.form['bet'])
-            if bet <= 0 or bet > player_balance:
-                return render_template('index.html', balance=player_balance, history=game_history, error="下注金额无效，请重新输入！")
-        except ValueError:
-            return render_template('index.html', balance=player_balance, history=game_history, error="请输入有效的数字！")
-
-        # 抽取点数
-        player_score = random.randint(1, 9)
-        banker_score = random.randint(1, 9)
-
-        # 判断胜负
-        if player_score > banker_score:
-            result = "玩家胜利！"
-            player_balance += bet
-        elif player_score < banker_score:
-            result = "庄家胜利！"
-            player_balance -= bet
-        else:
-            result = "平局！"
-
-        # 保存游戏记录
-        game_history.append({
-            "player_score": player_score,
-            "banker_score": banker_score,
-            "result": result,
-            "bet": bet
-        })
-
-        # 返回结果页面
-        return render_template('index.html', balance=player_balance, history=game_history)
-
-    # 初始页面
+@app.route('/')
+def index():
     return render_template('index.html', balance=player_balance, history=game_history)
+
+@app.route('/play', methods=['POST'])
+def play():
+    global player_balance
+    bet_amount = int(request.json.get('betAmount', 0))
+    target = request.json.get('target', 'player')
+
+    player_cards = [draw_card(), draw_card()]
+    banker_cards = [draw_card(), draw_card()]
+
+    player_score = sum([card[1] for card in player_cards]) % 10
+    banker_score = sum([card[1] for card in banker_cards]) % 10
+
+    if player_score > banker_score:
+        winner = 'player'
+    elif player_score < banker_score:
+        winner = 'banker'
+    else:
+        winner = 'tie'
+
+    balance_change = 0
+    if target == winner:
+        balance_change = bet_amount
+        player_balance += bet_amount
+    elif winner != 'tie':  # 平局不输不赢
+        balance_change = -bet_amount
+        player_balance -= bet_amount
+
+    result = {
+        'player_cards': [card[0] for card in player_cards],
+        'banker_cards': [card[0] for card in banker_cards],
+        'player_score': player_score,
+        'banker_score': banker_score,
+        'winner': winner,
+        'bet': bet_amount,
+        'balance_change': balance_change,
+    }
+    game_history.append(result)
+
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run(debug=True)
